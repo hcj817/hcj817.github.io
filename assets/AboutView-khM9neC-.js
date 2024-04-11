ccab9a9e0bf413b95aa3b795f38ed373603e33a2
hcj817 <162411607+hcj817@users.noreply.github.com>
2024-04-11 16:39:34 +0900

    Delete assets/AboutView-khM9neC-.js

--- assets/AboutView-khM9neC-.js
+++
@@ -1,1 +0,0 @@
-import{r as e,c as t,o,a as s}from"./index-XrwIRfcc.js";const a={class:"about"},c=s("h1",null,"안녕하세요오",-1),_=[c],u={__name:"AboutView",setup(r){return e(""),(n,i)=>(o(),t("div",a,_))}};export{u as default};
